2e31546a7f06de59c008a241a6e6ddc4aadedcd6
Ivan Noršić <154327833+Norsx@users.noreply.github.com>
2024-03-25 17:19:35 +0100

    Update kvadriranje_broja.py

--- kvadriranje_broja.py
+++ kvadriranje_broja.py
@@ -7,27 +7,17 @@ class MyNode(Node):
 
     def __init__(self):
         super().__init__("kvadriranje_broja")
-        self.counter_ = 0
         self.broj_subscriber_ = self.create_subscription(Int16, 'broj', self.broj_callback, 10)
-        self.cmd_vel_pub_ = self.create_publisher(Int16, 'kvadrat_broja', 10)
+        self.kvadrat_broja_publisher_ = self.create_publisher(Int16, 'kvadrat_broja', 10)
 
     def broj_callback(self, msg: Int16):
         broj = msg.data
-        x = Int16()
-        x.data = broj*broj
-        self.cmd_vel_pub_.publish(x)
-
-    def timer_callback(self):
-        self.counter_ += 1
-        x = Int16()
-        x.data = self.counter_
-        self.cmd_vel_pub_.publish(x)
+        kvadrat = broj * broj
+        self.kvadrat_broja_publisher_.publish(Int16(data=kvadrat))
 
 def main(args=None):
     rclpy.init(args=args)
-
     node = MyNode()
-
     rclpy.spin(node)
     rclpy.shutdown()
 
