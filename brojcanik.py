99f282325ed6372888df92898dc8c8b1684e044e
Ivan Noršić <154327833+Norsx@users.noreply.github.com>
2024-03-25 17:18:47 +0100

    Update brojcanik.py

--- brojcanik.py
+++ brojcanik.py
@@ -1,28 +1,23 @@
-#!/usr/bin/env python3
-import rclpy
+mport rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
 class MyNode(Node):
-
     def __init__(self):
         super().__init__("brojcanik")
-        self.counter_ = 0
+        self.counter = 0
         self.create_timer(1.0, self.timer_callback)
-
-        self.cmd_vel_pub_ = self.create_publisher(Int16, 'broj', 10)
+        self.publisher = self.create_publisher(Int16, 'broj', 10)
 
     def timer_callback(self):
-        self.counter_ += 1
-        x = Int16()
-        x.data = self.counter_
-        self.cmd_vel_pub_.publish(x)
+        self.counter += 1
+        msg = Int16()
+        msg.data = self.counter
+        self.publisher.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
-
     node = MyNode()
-
     rclpy.spin(node)
     rclpy.shutdown()
 
